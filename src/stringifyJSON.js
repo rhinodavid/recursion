4f93bb024538789366217e53c0fa48a14b7936d8
David Walsh <dawalsh@gmail.com>
2016-08-15 15:22:16 -0400

    Implement stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,59 @@
 
 // but you don't so you're going to write it from scratch:
 
+var trimTrailingComma = function(str) {
+  // takes a string and if the last character is a comma,
+  // returns the string without the trailing comma
+
+  if (str[str.length - 1] === ',') {
+    return str.slice(0, -1);
+  } else {
+    return str;
+  }
+};
+
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if (obj === undefined || obj === {}) {
+    return undefined;
+  }
+
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  var result;
+
+  if (Array.isArray(obj)) {
+    result = '[';
+    for (var i = 0; i < obj.length; i++) {
+      result = result + stringifyJSON(obj[i]) + ',';
+    }
+    result = trimTrailingComma(result);
+    result = result + ']';
+    return result;
+  }
+
+  if (typeof obj === 'object') {
+    result = '{';
+    for (var key in obj) {
+      if (key !== 'undefined' && key !== 'functions') {
+        result = result + '"' + key + '"' + ':';
+        result = result  + stringifyJSON(obj[key]) + ',';
+      }
+    }
+    result = trimTrailingComma(result);
+    result = result + '}';
+    return result;
+  }
+
 };
