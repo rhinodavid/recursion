73a5c19a14082bd9e5eac3cc43f8302c0df4108d
David Walsh <dawalsh@gmail.com>
2016-08-18 19:42:26 -0400

    Implement getInnerString

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -20,6 +20,53 @@ var trimWhitespace = function (str) {
   }
 };
 
+var checkBoundingChar = function(str, char) {
+  // checks to see if the str is bounded by the given
+  // char and it's pair.
+  // i.e. if str is "{abc}" and char is '{' the function
+  // will return true
+  // if str is "[1,2,3]" and char is '{' it will return false
+
+  var pair = {
+    '{' : '}',
+    '[' : ']',
+    '\'' : '\'',
+    '"' : '"'
+  };
+
+  if (str[0] === char && str[str.length - 1] === pair[char]) {
+    return true;
+  } else {
+    return false;
+  }
+
+};
+
+var getInnerString = function(str, char) {
+  // takes a string and returns the value inside a set of outside
+  // chars. for example, with str equal to "{'a':25}" and char
+  // equal to '{', getInnerString returns "'a':25"
+  //
+  // currently chars can be '{' or '['
+  
+  var str = trimWhitespace(str);
+
+  if ((checkBoundingChar(str, char))) {
+    // str isn't properly formed
+    return undefined;
+  }
+
+  return str.slice(1,-1);
+};
+
+////////// HELPER //////////
+
+
+
+
+
+////////////////////////////
+
 var parseJSON = function(json) {
   // your code goes here
 };
