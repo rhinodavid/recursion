d76ca75af9061f651246d9305eee06522b362fa2
David Walsh <dawalsh@gmail.com>
2016-08-18 18:08:10 -0400

    Implement trimWhitespace function Only trims spaces at the moment. Will need to include newline, tab, etc. eventually

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,29 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
+
+//////////// UTILITY ///////////////////
+
+var trimWhitespace = function (str) {
+  // takes a string and removes spaces from the beginning
+  // and end of the string
+
+  // currently only trims spaces. will need to eventually
+  // include tab, newline, etc
+  if (str[0] === ' ') {
+    return trimWhitespace(str.slice(1));
+  } else if (str[str.length -1] === ' ') {
+    return trimWhitespace(str.slice(0,-1));
+  } else {
+    return str;
+  }
+};
+
+
+
+
+
 var parseJSON = function(json) {
   // your code goes here
 };
+
